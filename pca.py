4ff94ddf3c247ab5b392bcacceb26bd99d31de48
ElifAyten <77199350+ElifAyten@users.noreply.github.com>
2025-07-08 15:49:13 +0200

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,3 +1,8 @@
+# rSLDS/pca.py
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
 __all__ = ["pca_summary", "plot_pca_cumsum"]
 
 # ----------------------------------------------------------------------
